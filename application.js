21adfe6aa92c506602fb4b021cadfb3bb19d10f2
Deepak Verma <deepak.23.verma@gmail.com>
2013-12-08 21:18:35 -0500

    Use JavaScript to build grid elements to cover image

--- application.js
+++ application.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 
+  for (var i = 0; i < 5842; i++) {
+    $("article").append("<p class=\"block\"></p>");
+  }
+
   $(".block").on("mouseenter", function(){
     $(this).addClass("hide")
   });
