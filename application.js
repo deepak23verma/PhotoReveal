381ed03dec82252e2a617f6094ab2f2c8a0fab8f
Deepak Verma <deepak.23.verma@gmail.com>
2013-12-18 23:17:39 -0500

    made it more kid friendly

--- application.js
+++ application.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
-
-  for (var i = 0; i < 465; i++) {
+for (var i = 0; i < 1023; i++) {
     $("article").append("<p class=\"block\"></p>");
   }
+
+$(document).ready(function(){
 
   $(".block").on("mouseenter", function(){
     $(this).addClass("hide")
